b2ac0f804cb76046f152d90a05ad9313151b966d
Manuel G-Estañ <manuel@softopusgarden.com>
2016-04-26 15:49:16 +0200

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'raccoon' do
+	pod 'Alamofire', '~> 3.3'
+end
+
+target 'raccoonTests' do
+
+end
+
---
+++ raccoon/raccoon.h
@@ -0,0 +1,19 @@
+//
+//  raccoon.h
+//  raccoon
+//
+//  Created by Manu on 26/4/16.
+//  Copyright © 2016 manuege. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for raccoon.
+FOUNDATION_EXPORT double raccoonVersionNumber;
+
+//! Project version string for raccoon.
+FOUNDATION_EXPORT const unsigned char raccoonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <raccoon/PublicHeader.h>
+
+
