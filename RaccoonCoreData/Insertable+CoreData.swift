45a152571060aae5bbc2f163ab62bebd19ea67f1
Manuel G-Estañ <manuel@softopusgarden.com>
2016-04-26 16:55:00 +0200

    create core data target

---
+++ RaccoonCoreData/Insertable+CoreData.swift
@@ -0,0 +1,47 @@
+//
+//  Insertable+CoreData.swift
+//  raccoon
+//
+//  Created by Manu on 21/4/16.
+//  Copyright © 2016 manuege. All rights reserved.
+//
+
+import CoreData
+import Raccoon
+import Groot
+
+extension NSManagedObjectContext: InsertContext {
+}
+
+
+/**
+ Insert managed object cotexts using Groot.
+ */
+extension NSManagedObject: Insertable {
+    
+    public typealias ContextType = NSManagedObjectContext
+    
+    public static func createOne(json: [String : AnyObject], context: NSManagedObjectContext) throws -> AnyObject? {
+        
+        guard let entityDescription = try? context.entityDescriptionForClass(self),
+            let entityName = entityDescription.name else {
+                return nil
+        }
+        
+        return try objectWithEntityName(entityName,
+                                         fromJSONDictionary: json,
+                                         inContext: context)
+    }
+    
+    public static func createMany(array: [AnyObject], context: NSManagedObjectContext) throws -> [AnyObject]? {
+        
+        guard let entityDescription = try? context.entityDescriptionForClass(self),
+            let entityName = entityDescription.name else {
+                return nil
+        }
+        
+        return try objectsWithEntityName(entityName,
+                                          fromJSONArray: array,
+                                          inContext: context)
+    }
+}
